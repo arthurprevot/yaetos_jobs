192854427c7e1db85d53fd48233ce8bd5b8f289f
Arthur Prevot <arthrprevot@gmail.com>
2023-12-27 11:27:58 +0100

    cleaning

--- jobs/gen_ai/albert_finetuned_inferences_job.py
+++ jobs/gen_ai/albert_finetuned_inferences_job.py
@@ -1,7 +1,5 @@
 from yaetos.etl_utils import ETL_Base, Commandliner, Path_Handler
-# from transformers import file_utils
 import tensorflow as tf
-# import numpy as np
 import pandas as pd
 from albert_finetune_job import Job as job_af
 
@@ -17,10 +15,6 @@ class Job(ETL_Base):
         path = Path_Handler(path, self.jargs.base_path, self.jargs.merged_args.get('root_path')).expand_later()
         model = job_af.reload_model(path)
 
-        # from transformers import TFAlbertForSequenceClassification
-        # model = TFAlbertForSequenceClassification.from_pretrained(job_af.MODEL_NAME)
-
-
         evaluations = self.predict_all(model, text_to_classify)
         return evaluations
 
@@ -28,8 +22,6 @@ class Job(ETL_Base):
         texts = text_to_classify['text'].tolist()
         texts_proc = job_af.preprocess(texts, job_af.MODEL_NAME)
         predictions = job_af.predict(model, texts_proc)
-        # real = text_to_classify['classification'].tolist()
-        # return pd.DataFrame({'texts': texts, 'predictions': predictions, 'real': real})
         return pd.DataFrame({'texts': texts, 'predictions': predictions})
     
 
