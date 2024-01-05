6e0066705af454ae54a0422f7425702826ed9fef
Arthur Prevot <arthrprevot@gmail.com>
2024-01-05 13:02:30 +0100

    fix

--- jobs/climate/climate_trace_asset_dimension_job.py
+++ jobs/climate/climate_trace_asset_dimension_job.py
@@ -7,8 +7,8 @@ class Job(ETL_Base):
     def transform(self, climate_trace):
         old_token = 'access_token=pk.eyJ1IjoiZWFydGhyaXNlIiwiYSI6ImNsN3NkMGkzdjBibWYzb2xhZndnNDc1d20ifQ.e4eN5Ee2g4G-zYcOgM-VQg'  # hardcoded in data probably by mistake.
         df = climate_trace.drop(['Emissions', 'Owners', 'Confidence'], axis=1)
-        df['latitude'] = df['Centroid'].apply(lambda ce: ce['Geometry'][0])
-        df['longitude'] = df['Centroid'].apply(lambda ce: ce['Geometry'][1])
+        df['latitude'] = df['Centroid'].apply(lambda ce: ce['Geometry'][1])
+        df['longitude'] = df['Centroid'].apply(lambda ce: ce['Geometry'][0])
         df['Thumbnail_post'] = df['Thumbnail'].apply(lambda ce: ce.replace(old_token, ''))
         df = df.drop(['Thumbnail'], axis=1)
         return df
