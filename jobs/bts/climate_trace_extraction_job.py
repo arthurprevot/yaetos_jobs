32f350b25e076c49870a6ead8189d34fae7ed5cf
Arthur Prevot <arthrprevot@gmail.com>
2025-06-03 01:16:02 +0200

    tweaked to make it work. Not cleaned

--- jobs/bts/climate_trace_extraction_job.py
+++ jobs/bts/climate_trace_extraction_job.py
@@ -1,6 +1,6 @@
 """
 More details about the data ingested at https://climatetrace.org/
-Details about the API: https://api.climatetrace.org/v4/swagger/index.html
+Details about the API: https://api.climatetrace.org/v6/swagger/index.html
 """
 from yaetos.etl_utils import ETL_Base, Commandliner
 import pandas as pd
@@ -11,6 +11,8 @@ class Job(ETL_Base):
     def transform(self):
         # Code in pandas so limited in output size.
         countries = 'USA'
+        continent = 'Europe'
+        countries = continent
         AssetCount, Emissions = self.get_assets_size(countries=countries)
         assets_per_page = 100
         number_pages = AssetCount // assets_per_page + 1
@@ -25,9 +27,9 @@ class Job(ETL_Base):
         return df
 
     def get_assets(self, countries=None, limit=None, offset=None):
-        url = "https://api.climatetrace.org/v4/assets"
+        url = "https://api.climatetrace.org/v6/assets"
         args = '?'
-        args += f'countries={countries}&' if countries else ''
+        args += f'continents={countries}&' if countries else ''
         args += f'limit={limit}&' if limit else ''
         args += f'offset={offset}&' if offset else ''
         # Note: tested sectors, subsectors and year params but they didn't work
@@ -38,15 +40,17 @@ class Job(ETL_Base):
         return assets
 
     def get_assets_size(self, countries=None, limit=None):
-        url = "https://api.climatetrace.org/v4/assets/emissions"
+        url = "https://api.climatetrace.org/v6/assets/emissions"
         args = '?'
-        args += f'countries={countries}&' if countries else ''
+        args += f'continents={countries}&' if countries else ''
         url += args
-        get_size_fct = lambda data: None
-        __, data, ___ = self.api_pull(url, get_size_fct)
+        dummy_size_fct = lambda data: None
+        __, data, ___ = self.api_pull(url, dummy_size_fct)
+        assert data is not None
         keys = list(data.keys())
         assert len(keys) == 1
         rows = data[keys[0]]
+        # import ipdb; ipdb.set_trace()
         AssetCount = sum([item['AssetCount'] for item in rows])
         Emissions = sum([item['Emissions'] for item in rows])  # TODO: make sure it can be sumed.
         return AssetCount, Emissions
