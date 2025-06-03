df49a66f6671ab048c7af9ca8f11a5fd69892795
Arthur Prevot <arthrprevot@gmail.com>
2025-06-03 02:22:01 +0200

    adapted dimension to deal with changes in api (year info not in emission blob anymore)

--- jobs/bts/climate_trace_asset_dimension_job.py
+++ jobs/bts/climate_trace_asset_dimension_job.py
@@ -4,11 +4,16 @@ from yaetos.etl_utils import ETL_Base, Commandliner
 class Job(ETL_Base):
     def transform(self, climate_trace):
         old_token = 'access_token=pk.eyJ1IjoiZWFydGhyaXNlIiwiYSI6ImNsN3NkMGkzdjBibWYzb2xhZndnNDc1d20ifQ.e4eN5Ee2g4G-zYcOgM-VQg'  # hardcoded in data probably by mistake.
-        df = climate_trace.drop(['Emissions', 'Owners', 'Confidence'], axis=1)
+        df = climate_trace.drop(['EmissionsSummary', 'Owners', 'Confidence', 'year'], axis=1)
         df['latitude'] = df['Centroid'].apply(lambda ce: ce['Geometry'][1])
         df['longitude'] = df['Centroid'].apply(lambda ce: ce['Geometry'][0])
         df['Thumbnail_post'] = df['Thumbnail'].apply(lambda ce: ce.replace(old_token, ''))
-        df = df.drop(['Thumbnail'], axis=1)
+        df = df.drop(['Thumbnail', 'SectorRanks', 'Centroid', 'offset_batch'], axis=1)
+
+        # drop duplicates
+        variable_field = 'year'
+        key_fields = [col for col in df.columns if col != variable_field]
+        df = df[key_fields].drop_duplicates()
         return df
 
 
